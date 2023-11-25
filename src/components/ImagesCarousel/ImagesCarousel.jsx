1d1cd2ebbc97cbd456d263aa45649c6e4ea6724b
Santiago Agudelo <sagudecod97@gmail.com>
2023-11-24 20:27:43 -0500

    Added next and prev handlers, also styles, left beyond media-mds stlyles to be done

--- src/components/ImagesCarousel/ImagesCarousel.jsx
+++ src/components/ImagesCarousel/ImagesCarousel.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './ImagesCarousel.scss';
 
 const CarouselItem = (props) => {
@@ -13,15 +13,19 @@ const CarouselItem = (props) => {
 
   return (
     <div
-      className='carousel-item'
+      className={`
+      carousel-item
+      ${isItemActive ? '' : '--hidden'}
+      `}
       role='group'
       aria-roledescription='slide'
       aria-label={`${itemIndex} de ${itemsLength}`}
       aria-hidden={isItemActive}
       id={id}
     >
-      <div className='carousel-item__image'>
+      <div className='carousel-item__image-container'>
         <img
+          className='carousel-item__image-tag'
           src={imageSrc}
           alt={alt}
         />
@@ -36,6 +40,43 @@ const ImagesCarousel = (props) => {
     imagesArray,
   } = props;
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [multipleSlides, setMultipleSlides] = useState(false);
+  const [activeSlides, setActiveSlides] = useState([]);
+
+  const nextSlideHandler = () => {
+    if (currentIndex === imagesArray.length - 1) {
+      return setCurrentIndex(0);
+    }
+
+    setCurrentIndex(currentIndex + 1);
+  }
+
+  const previousSlideHandler = () => {
+    if (currentIndex === 0) {
+      return setCurrentIndex(imagesArray.length - 1);
+    }
+
+    setCurrentIndex(currentIndex - 1);
+  }
+
+  useEffect(() => {
+    if (windowWidth >= 991) {
+      setMultipleSlides(true)
+    } else {
+      setMultipleSlides(false);
+    }
+
+    const updateWindowWidth = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', updateWindowWidth);
+
+    return () => {
+      window.removeEventListener('resize', updateWindowWidth);
+    };
+  }, [windowWidth, multipleSlides]);
 
   return (
     <div 
@@ -47,21 +88,21 @@ const ImagesCarousel = (props) => {
         className='images-carousel__controls'
       >
         <button
-          className='images-carousel__controls-prev'
+          className='images-carousel__controls-control --prev'
           aria-controls='carousel-images-items'
           aria-label='Imagen Anterior'
-          onClick={() => console.log('Prev')}
+          onClick={previousSlideHandler}
         >
-          ❮
+          <span>❮</span>
         </button>
 
         <button
-          className='images-carousel__controls-next'
+          className='images-carousel__controls-control --next'
           aria-controls='carousel-images-items'
           aria-label='Imagen Siguiente'
-          onClick={() => console.log('Next')}
+          onClick={nextSlideHandler}
         >
-          ❯
+          <span>❯</span>
         </button>
       </div>
 
--- src/data/objectCarouselImagesData.js
+++ src/data/objectCarouselImagesData.js
@@ -1,27 +1,28 @@
 import SeniorWoman from '../assets/images/senior-woman.jpg';
+import Grandma from '../assets/images/grandma.jpg';
 
 const objectCarouselImagesData = {
   label: 'Carrusel de Imágenes ',
   imagesArray: [
     {
-      imageSrc: SeniorWoman,
+      imageSrc: Grandma,
       alt: 'Imagen de señora sonriendo',
-      id: ((Math.random * 10) * Math.random()),
+      id: ((Math.random() * 10) * Math.random()),
     },
     {
       imageSrc: SeniorWoman,
       alt: 'Imagen de señora sonriendo',
-      id: ((Math.random * 10) * Math.random()),
+      id: ((Math.random() * 10) * Math.random()),
+    },
+    {
+      imageSrc: Grandma,
+      alt: 'Imagen de señora sonriendo',
+      id: ((Math.random() * 10) * Math.random()),
     },
     {
       imageSrc: SeniorWoman,
       alt: 'Imagen de señora sonriendo',
-      id: ((Math.random * 10) * Math.random()),
-    },
-    {
-      imageSrc: SeniorWoman,
-      alt: 'Imagen de señora sonriendo',
-      id: ((Math.random * 10) * Math.random()),
+      id: ((Math.random() * 10) * Math.random()),
     },
   ]
 };
