acd18c287655e6e786ecc88b35a114d80ccee650
Santiago Agudelo <sagudecod97@gmail.com>
2023-12-01 23:34:45 -0500

    Erased development setup for images carousel

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import './App.scss';
 import Header from './components/Header/Header.jsx';
 import Footer from './components/Footer/Footer.jsx';
 
-import objectCarouselImagesData from './data/objectCarouselImagesData.js';
-
-import ImagesCarousel from './components/ImagesCarousel/ImagesCarousel.jsx';
-
 function App() {
   return (
     <div
@@ -16,10 +12,6 @@ function App() {
         className='main' 
         role='main'
       >
-        <ImagesCarousel
-          label={objectCarouselImagesData.label}
-          imagesArray={objectCarouselImagesData.imagesArray}
-        />
       </main>
       <Footer />
     </div>
